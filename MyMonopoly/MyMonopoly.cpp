8b544377f3c420289597500474bd2fc4b46d7f49
mrhpshka@gmail.com <mrhpshka@gmail.com>
2020-06-30 14:22:45 +0700

    Сделал начало кода монополии.

--- MyMonopoly/MyMonopoly.cpp
+++ MyMonopoly/MyMonopoly.cpp
@@ -1,12 +1,72 @@
-﻿
-
-#include <iostream>
-
+﻿#include <iostream>
+#include <ctime>
+#include "windows.h"
+#include <string>
+#include <vector>
 using namespace std;
+
+class Business {
+
+    bool Occupied = false;
+
+public:
+};
+
+class Cars {
+
+    bool Occupied = false;
+
+public:
+};
+
+class Movies {
+
+    bool Occupied = false;
+
+public:
+};
+
+class Question {
+
+};
+
+class Jail {
+
+};
+
+class Penalty {
+
+};
+
+class Map {
+
+    int NumberPlayers;
+
+public:
+
+    string GetInfoPosition(int NumberPosition) {
+        if (NumberPosition);
+    }
+
+};
+
+class Player {
+
+    int Money = 15000;
+
+public:
+
+    void MakeMotion() {
+
+    }
+
+};
 
 int main()
 {
-	cout << "Я сделал изменение";
-	return 1;
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    int NumberPlayers = 2;
+    vector <Player> Players(NumberPlayers);
+    Map Game;
 }
-
