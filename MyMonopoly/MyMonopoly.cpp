81ac4bd9f27f9df0532adf185277a3fc6b48bdb3
Nikita Salkov <Nikita_8.kz@mail.ru>
2020-07-08 17:10:47 +0700

    Изменил передачу объектов класса Player, теперь переда происходит по указателю. Добавил в Player метод makeMotion. В main добавил список игроков, этот список будет использоваться в цикле игры. в Player добавил поле Possition и его геттер и геттер для поля inJail.

--- MyMonopoly/MyMonopoly.cpp
+++ MyMonopoly/MyMonopoly.cpp
@@ -3,6 +3,7 @@
 #include "windows.h"
 #include <string>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -13,14 +14,30 @@ int MakeRandomNumberForCube() {
 class Business;
 
 class Player {
-    int money = 15000;
-    int sleepMotions = 0;
+    int money;
+    int sleepMotions;
     int number;
-    bool inJail = false;
-    bool Alive = true;
+    int possition;
+    bool inJail;
+    bool Alive;
     vector <Business> Businesses;
 public:
+    Player(int number)
+    {
+        money = 15000;
+        sleepMotions = 0;
+        this->number = number;
+        possition = 1;
+        inJail = false;
+        Alive = true;
+    }
     void makeMotion() {
+        int cube1, cube2;
+        cube1 = MakeRandomNumberForCube();
+        cube2 = MakeRandomNumberForCube();
+        cout << "На первом кубике выпало число: " << cube1 << " , на втором: " << cube2 << endl;
+        if (cube1 + cube2 > 39) possition += (cube1 + cube2) % 40;
+        else possition += cube1 + cube2;
     }
 
     void plusBusiness(int NumberCard) {
@@ -32,9 +49,16 @@ public:
     int getNumber() {
         return(number);
     }
-
     void setNumber(int Number) {
         this->number = Number;
+    }
+    int getPossition()
+    {
+        return possition;
+    }
+    bool getInJail()
+    {
+        return inJail;
     }
     friend class Question;
     friend class Jail;
@@ -49,7 +73,7 @@ protected:
     }
 public:
     int NumberCard;
-    virtual void Action(Player player) = 0;
+    virtual void Action(Player* player) = 0;
     //virtual ~Card() = default;
 };
 
@@ -74,14 +98,15 @@ public:
         this->BusinessName = BusinessName;
     }
 
-    void Action(Player player) override
+    void Action(Player* player) override
     {
         string PlayerAnswer;
-        cout << "Хотите ли купить бинес " << BusinessName << " за " << cost << ". Он будет приносить " << profit << " при посещении. Если вы не купите бизнес, то он будет выставлен на аукцион.\n1) Да\n2) Нет";
+        cout << "Хотите ли купить бинес " << BusinessName << " за " << cost << ". Он будет приносить " << profit 
+            << " при посещении. Если вы не купите бизнес, то он будет выставлен на аукцион.\n1) Да\n2) Нет\nОтвет: ";
         cin >> PlayerAnswer;
         if (PlayerAnswer == "1") {
-            player.plusBusiness(NumberCard);
-            Owner = player.getNumber();             //Если забагается, то нужно будет сделать функцию равенства в классе Player
+            (*player).plusBusiness(NumberCard);
+            Owner = (*player).getNumber();             //Если забагается, то нужно будет сделать функцию равенства в классе Player
             Occupied = true;
         }
         else {
@@ -96,20 +121,20 @@ public:
 };
 
 class Jail : public Card {
-    void Arrest(Player player) {
-        player.inJail = true;
-        player.sleepMotions = 3;
+    void Arrest(Player* player) {
+        (*player).inJail = true;
+        (*player).sleepMotions = 3;
     }
 
-    void Freedom(Player player) {
+    void Freedom(Player* player) {
         string UserAnswer;
-        cout << "Хотите ли выйти из тюрьмы?\n1)Да\n2)Нет" << endl;
+        cout << "Хотите ли выйти из тюрьмы?\n1)Да\n2)Нет\nОтвет: " << endl;
         cin >> UserAnswer;
         if (UserAnswer == "1") {
-            if (player.money > 500) {
-                player.money -= 500;
-                player.inJail = false;
-                player.sleepMotions = 0;
+            if ((*player).money > 500) {
+                (*player).money -= 500;
+                (*player).inJail = false;
+                (*player).sleepMotions = 0;
             }
             else cout << "У вас нет денег!";
         }
@@ -117,16 +142,16 @@ class Jail : public Card {
             int CubeOne = MakeRandomNumberForCube();
             int CubeTwo = MakeRandomNumberForCube();
             if (CubeOne == CubeTwo) {
-                player.inJail = false;
-                player.sleepMotions = 0;
+                (*player).inJail = false;
+                (*player).sleepMotions = 0;
             }
         }
     }
 public:
     Jail(int NumberCard) : Card(NumberCard)
     { }
-    void Action(Player player) override {
-        if (player.inJail) {
+    void Action(Player* player) override {
+        if ((*player).inJail) {
             Freedom(player);
         }
         else Arrest(player);
@@ -134,29 +159,29 @@ public:
 };
 
 class Question : public Card {
-    void DoQuestion(int NumberQuestion, Player Player) {
+    void DoQuestion(int NumberQuestion, Player* player) {
         if (NumberQuestion == 0) {
-            Player.money -= 1000;
+            (*player).money -= 1000;
         }
         if (NumberQuestion == 1) {
-            Player.money += 500;
+            (*player).money += 500;
         }
         if (NumberQuestion == 2) {
-            Player.money += 2000;
+            (*player).money += 2000;
         }
         // и т.д., придумаем карточки по ходу событий
     }
 public:
     Question(int NumberCard) : Card(NumberCard)
     { }
-    void Action(Player Player) {
+    void Action(Player *player) {
         int NumberQuestion = rand() % 10;       // кол-во карточек нужно будет увеличить
-        DoQuestion(NumberQuestion, Player);
+        DoQuestion(NumberQuestion, player);
     }
 };
 
 class Casino : public Card {
-    void PlayCasino(Player player) {
+    void PlayCasino(Player *player) {
         char AnswerPlayer[10];
         ReQuestion:
         cout << "Выберите кол-во пуль (1-5): ";
@@ -165,14 +190,14 @@ class Casino : public Card {
         int CasinoNumber = rand() % 100 + 1;
         if (CasinoNumber > (atoi(AnswerPlayer) * 18)) {
             cout << "Поздравляем! Ваш выигрыш: " << atoi(AnswerPlayer) * 1250 << endl;
-            player.money += atoi(AnswerPlayer) * 1250;
+            (*player).money += atoi(AnswerPlayer) * 1250;
         }
-        else { cout << "Вам не повезло, вы умерли.(((\n"; player.Alive = false; }
+        else { cout << "Вам не повезло, вы умерли.(((\n"; (*player).Alive = false; }
     }
 public:
     Casino (int NumberCard) : Card(NumberCard)
     { }
-    void Action(Player player) override {
+    void Action(Player* player) override {
         string AnswerPlayer;
         cout << "Хотите ли вы сыграть в казино?\n1) Да\n2) Нет" << endl;
         cin >> AnswerPlayer;
@@ -186,23 +211,26 @@ class Map {
 private:
     int numberPlayers;
     vector<Card*> vectorOfCards;
-    void getQuestion(int numberOfCard) {
-        for (int i = 0; i < vectorOfCards.size(); i++) {
-            if (vectorOfCards[i]->NumberCard == numberOfCard) {
-                vectorOfCards[i]->Action();             //не совсем понял, зачем
-                break;
-            }
-        }
-    }
+   
 
 public:
     Map(int numberPlayers) {
         this->numberPlayers = numberPlayers;
     }
 
+
     void initCard(Card* newCard)
     {
         vectorOfCards.push_back(newCard);
+    }
+    void getQuestion(Player* player) {
+        for (int i = 0; i < vectorOfCards.size(); i++) {
+            if (vectorOfCards[i]->NumberCard == (*player).getNumber())
+            {
+                vectorOfCards[i]->Action((*player));             //не совсем понял, зачем
+                break;
+            }
+        }
     }
     Card* getCard(int index) {
         return vectorOfCards[index];
@@ -223,16 +251,31 @@ public:
 
 int main()
 {
+    srand(time(NULL));
     setlocale(LC_ALL, "Russian");
+    list <Player*> listOfPlayers;
     Business b(1, 2, 3, "gg", 4);
     Business c(2, 3, 4, "oo", 5);
     Jail j(7);
-    Map map(2);
+    Map map(4);
+    int currentPlayerNumber = 0;
     map.initCard(&b);
     map.initCard(&c);
     map.initCard(&j);
-    cout << map.getCard(0)->NumberCard << endl;
-    cout << map.getCard(2)->NumberCard;
+    listOfPlayers.push_back(new Player(1));
+    listOfPlayers.push_back(new Player(2));
+    listOfPlayers.push_back(new Player(3));
+    listOfPlayers.push_back(new Player(4));
+    auto currentPlayer = listOfPlayers.begin();
+    while(!listOfPlayers.empty())
+    {
+        if (!(*currentPlayer)->getInJail())
+        {
+            (*currentPlayer)->makeMotion();
+            map.getQuestion((*currentPlayer));
+        }
+        currentPlayer++;
+    }
     return 1;
 
 }
